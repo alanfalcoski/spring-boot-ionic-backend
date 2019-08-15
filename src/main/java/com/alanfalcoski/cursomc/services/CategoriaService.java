f59fce3c1f19789dcb5c046c13295328dc6b8ea9
Alan Rodrigues <falcoskialan@gmail.com>
2019-08-15 00:14:22 -0300

    Deletando categoria com exceção personalizada em caso de constraint

--- src/main/java/com/alanfalcoski/cursomc/services/CategoriaService.java
+++ src/main/java/com/alanfalcoski/cursomc/services/CategoriaService.java
@@ -4,9 +4,10 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
+import org.springframework.dao.DataIntegrityViolationException;
 import com.alanfalcoski.cursomc.domain.Categoria;
 import com.alanfalcoski.cursomc.repositories.CategoriaRepository;
+import com.alanfalcoski.cursomc.services.exceptions.DataIntegrityException;
 import com.alanfalcoski.cursomc.services.exceptions.ObjectNotFoundException;
 
 
@@ -30,5 +31,15 @@ public class CategoriaService {
 	public Categoria update(Categoria obj) {
 		find(obj.getId());
 		return repo.save(obj);
+	}
+	
+	public void delete (Integer id) {
+		find(id);
+		try {
+			repo.deleteById(id);
+		}
+		catch(DataIntegrityViolationException e){
+			throw new DataIntegrityException("Não é possível deletar uma categoria com produtos.");
+		}
 	}
 }
