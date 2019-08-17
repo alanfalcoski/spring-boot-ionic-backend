4acdc50a690e20efd9e839b4d36686a81cdb067d
Alan Rodrigues <falcoskialan@gmail.com>
2019-08-17 00:04:28 -0300

    Validação sintática com Java Beans

--- src/main/java/com/alanfalcoski/cursomc/services/CategoriaService.java
+++ src/main/java/com/alanfalcoski/cursomc/services/CategoriaService.java
@@ -11,6 +11,7 @@ import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.alanfalcoski.cursomc.domain.Categoria;
+import com.alanfalcoski.cursomc.dto.CategoriaDTO;
 import com.alanfalcoski.cursomc.repositories.CategoriaRepository;
 import com.alanfalcoski.cursomc.services.exceptions.DataIntegrityException;
 import com.alanfalcoski.cursomc.services.exceptions.ObjectNotFoundException;
@@ -55,5 +56,9 @@ public class CategoriaService {
 	public Page<Categoria> findPage(Integer page, Integer linesPerPage, String orderBy, String direction){
 		PageRequest pageRequest= PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);		
 		return repo.findAll(pageRequest);			
+	}
+	
+	public Categoria fromDTO(CategoriaDTO objDto) {
+		return new Categoria(objDto.getId(), objDto.getNome());
 	}
 }
