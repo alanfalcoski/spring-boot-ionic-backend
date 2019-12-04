70a5a0cd1a84b36537faaf0fa19019d20273c729
Alan Rodrigues <falcoskialan@gmail.com>
2019-12-04 00:01:44 -0300

    Criando profile de desenvolvimento

---
+++ src/main/java/com/alanfalcoski/cursomc/config/DevConfig.java
@@ -0,0 +1,33 @@
+package com.alanfalcoski.cursomc.config;
+
+import java.text.ParseException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import com.alanfalcoski.cursomc.services.DBService;
+
+@Configuration
+@Profile("dev")
+public class DevConfig {
+
+	@Autowired
+	private DBService dbService;
+	
+	@Value("${spring.jpa.hibernate.ddl-auto}")
+	private String strategy;
+	
+	@Bean
+	public boolean instantiateDatabase() throws ParseException {
+		/* Estratégia para não instanciar o banco de dados duas vezes */
+		if (!"create".equals(strategy)) {
+			return false;
+		}
+		
+		dbService.instantiateTesteDatabase();
+		return true;
+	}
+}
