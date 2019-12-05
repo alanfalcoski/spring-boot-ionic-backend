7761d3232268b4d31c638086e4d8f14ff82b4073
Alan Rodrigues <falcoskialan@gmail.com>
2019-12-04 22:47:26 -0300

    Implementando SmtpEmailService com servidor do Google [fail]

--- src/main/java/com/alanfalcoski/cursomc/services/PedidoService.java
+++ src/main/java/com/alanfalcoski/cursomc/services/PedidoService.java
@@ -70,7 +70,7 @@ public class PedidoService {
 		}
 		
 		itemPedidoRepository.saveAll(obj.getItens());
-		System.out.println(obj);
+		
 		emailService.sendOrderConfirmationEmail(obj);
 		
 		return obj;
