013e30548761cbd10f661c2abc445a4ee5081ad8
Alan Rodrigues <falcoskialan@gmail.com>
2019-12-14 17:30:51 -0300

    Salvando arquivo no Amazon S3

--- src/main/java/com/alanfalcoski/cursomc/CursomcApplication.java
+++ src/main/java/com/alanfalcoski/cursomc/CursomcApplication.java
@@ -1,18 +1,26 @@
 package com.alanfalcoski.cursomc;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.alanfalcoski.cursomc.services.S3Service;
+
 @SpringBootApplication
 public class CursomcApplication implements CommandLineRunner{	
+	
+	@Autowired
+	private S3Service s3Service;
+	
 	
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
 	}
 
 	@Override
-	public void run(String... args) throws Exception {						
+	public void run(String... args) throws Exception {	
+		s3Service.uploadFile("C:\\Users\\alang\\Pictures\\sd.jpg");
 	}
 
 }
